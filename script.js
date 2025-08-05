e47626a0309d993ae36d666158d4f9d6e4e966e5
Barnabas Shivayilu <shivayilubarnabas@gmail.com>
2025-08-05 15:10:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,16 @@ function showConfetti() {
     origin: { y: 0.6 }
   });
 }
+const downloadLinks = document.querySelectorAll(".poster a");
+const loadingScreen = document.getElementById("loading-screen");
 
+downloadLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    loadingScreen.style.display = "flex";
+    setTimeout(() => {
+      loadingScreen.style.display = "none";
+      window.location.href = link.href;
+    }, 3000);
+  });
+});
