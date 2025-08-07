8a3ce4e9ba1276d6bec89a6bdb0b3a99506cbabf
Barnabas Shivayilu <shivayilubarnabas@gmail.com>
2025-08-07 13:07:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,4 +36,34 @@
       });
     }
   }, { once: true });
+
+function startConfetti() {
+  const duration = 5 * 1000; // 5 seconds
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 1000 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function() {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+    
+    confetti({
+      particleCount,
+      angle: randomInRange(55, 125),
+      spread: randomInRange(50, 100),
+      origin: { x: Math.random(), y: Math.random() - 0.2 },
+      ...defaults
+    });
+  }, 250);
+}
+
+
 </script>
